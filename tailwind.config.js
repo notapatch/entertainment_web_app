0ca96558fc21eb6d8844a40a0c174ed7dfc151dd
Richard <richard.wigley@gmail.com>
2022-03-17 22:46:40 +0000

    CSS color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,45 @@ module.exports = {
     './app/assets/images/**/*.svg',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    extend: {
+      backgroundColor: {
+        skin: {
+          background:       'var(--color-background)',
+          surface:          'var(--color-surface)',
+          primary:          'var(--color-primary)',
+          'primary-hover':  'var(--color-primary-hover)',
+        }
+      },
+      borderColor: {
+        skin: {
+          'surface-text':           'var(--color-surface-text)',
+          'surface-text-secondary': 'var(--color-surface-text-secondary)',
+        }
+      },
+      caretColor: {
+        skin: {
+         'primary':         'var(--color-primary)',
+        }
+      },
+      textColor: {
+        skin: {
+          'background-text':           'var(--color-background-text)',
+          'background-text-muted':     'var(--color-background-text-muted)',
+          'background-text-secondary': 'var(--color-background-text-secondary)',
+          'background-text-error':     'var(--color-background-text-error)',
+
+          'surface-text':              'var(--color-surface-text)',
+          'surface-text-error':        'var(--color-primary)',
+          'surface-text-muted':        'var(--color-surface-text-muted)',
+          'surface-text-secondary':    'var(--color-surface-text-secondary)',
+
+          'primary':                   'var(--color-primary)',
+          'primary-text':              'var(--color-primary-text)',
+          'primary-text-hover':        'var(--color-primary-text-hover)',
+        }
+      }
+    }
+  }
 }
