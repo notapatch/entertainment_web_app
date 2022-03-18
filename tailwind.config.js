d77736de48f4d8ad7e553ad5bcce9e65b06733b7
Richard <richard.wigley@gmail.com>
2022-03-18 11:40:01 +0000

    CSS search for css classes within components and svg images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   content: [
+    './app/components/**/*',
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
+    './app/assets/images/**/*.svg',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ]
