df90ac7f228c6f7023504df987b808de81c605f3
Richard <richard.wigley@gmail.com>
2022-03-21 15:25:27 +0000

    ShowObject wrapper around json data

---
+++ lib/objects/show_object.rb
@@ -0,0 +1,39 @@
+class ShowObject
+  def initialize(data)
+    @data = data
+  end
+
+  def bookmarked
+    @data["isBookmarked"]
+  end
+
+  def category
+    enumerize(@data["category"])
+  end
+
+  def title
+    @data["title"]
+  end
+
+  def rating
+    enumerize(@data["rating"])
+  end
+
+  def regular_image_path
+    @data.dig("thumbnail", "trending", "large")
+  end
+
+  def trending
+    @data["isTrending"]
+  end
+
+  def year
+    @data["year"]
+  end
+
+  private
+
+  def enumerize(category)
+    category.parameterize(separator: "_").to_sym
+  end
+end
