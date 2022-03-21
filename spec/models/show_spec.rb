ef30878b4446cce6d149d1dda1e8d2690d5d1d9d
Richard <richard.wigley@gmail.com>
2022-03-21 16:08:36 +0000

    Show validation added

--- spec/models/show_spec.rb
+++ spec/models/show_spec.rb
@@ -1,5 +1,10 @@
 require "rails_helper"
 
 RSpec.describe Show, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#validates" do
+    it { is_expected.to validate_presence_of(:category) }
+    it { is_expected.to validate_presence_of(:rating) }
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:year) }
+  end
 end
