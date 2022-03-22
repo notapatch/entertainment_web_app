dad65cd7a43bdecf4c8827fcb2c21de9e7d891b4
Richard <richard.wigley@gmail.com>
2022-03-22 18:43:23 +0000

    Devise
    From Getting Started
    - gem 'devise'
    - rails generate devise:install
    - config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
    - rails generate devise Customer
    - rails db:migrate
    - root already added
    
    FactoryBot
    - FFaker attributes for email

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :customers, path: "customers"
   root "landing_pages#show"
 
   resource :landing_page, only: [:show]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,11 @@
+include FactoryBot::Syntax::Methods
 
+Rails.logger.debug "Seed starting"
+
+Customer.delete_all
+
+1.upto(4).each do |_index|
+  create(:customer)
+end
+
+Rails.logger.debug "Seed ending"
