7c7023a249252659305ea95d31609c0df0ea4faf
Richard <richard.wigley@gmail.com>
2022-03-29 04:08:37 -0700

    Customer has avatar

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -5,5 +5,12 @@ FactoryBot.define do
     end
     password { "password" }
     password_confirmation { "password" }
+
+    trait :avatared do
+      after(:build) do |customer|
+        customer.avatar.attach(io: File.open(Rails.root.join("_starter/assets/user_images/image-anne.jpg")),
+                               filename: "image-anne.jpg", content_type: "image/jpeg")
+      end
+    end
   end
 end
