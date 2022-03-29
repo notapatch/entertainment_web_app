6c21baad533b56aea86f0fbf8ee8f1365be4df73
Richard <richard.wigley@gmail.com>
2022-03-29 04:08:37 -0700

    Customer has avatar

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,13 @@ Rails.logger.debug "Seed starting"
 
 Customer.delete_all
 
+path = Rails.root.join("_starter/assets/user_images/")
+images = Dir.children(path)
+
 1.upto(4).each do |_index|
-  create(:customer)
+  customer = create(:customer)
+  image = images.pop
+  customer.avatar.attach(io: File.open(path + image), filename: image, content_type: "image/jpg")
 end
 
 Rails.logger.debug "Seed ending"
